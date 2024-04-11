b2273809492728539b32045dd3238722db045cf3
nbortz <122999602+nbortz@users.noreply.github.com>
2024-04-11 11:10:38 -0400

    Fixed Errors NB

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,14 @@
 a, b = 0, 1
-sum_even = 0;
+#removed semicolon from line 3
+sum_even = 0
 # Only sum fibonacci values up to 4,000,000
-while b <= 4000000:
-    if b / 2 == 0:
+#changed <= to < 
+while b < 4000000:
+    #changed / to %
+    if b % 2 == 0:
         sum_even += b
-    a, b = b, b
+    #changed b, b to b, a+b
+    a, b = b, a + b
 
 print(sum_even)
 
